01d35a3226167694024458db2a597e02fe120595
cesine <cesine@yahoo.com>
2014-09-12 11:49:15 -0400

    adding a pageradaptor

---
+++ library/src/com/briangriffey/notebook/PageTurnPagerAdaptor.java
@@ -0,0 +1,26 @@
+package com.briangriffey.notebook;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PageTurnPagerAdaptor extends FragmentStatePagerAdapter {
+
+	public PageTurnPagerAdaptor(FragmentManager fm) {
+		super(fm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
