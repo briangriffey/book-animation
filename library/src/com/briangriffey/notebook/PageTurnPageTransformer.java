2cb4f24340a8ae61d62fb9546894622370a16483
cesine <cesine@yahoo.com>
2014-09-12 14:57:28 -0400

    page flip transformation sample
    from https://gist.github.com/JMPergar/6eb76173a5f7941fcc6f

--- library/src/com/briangriffey/notebook/PageTurnPageTransformer.java
+++ library/src/com/briangriffey/notebook/PageTurnPageTransformer.java
@@ -10,38 +10,45 @@ import android.view.View;
  *
  */
 public class PageTurnPageTransformer implements ViewPager.PageTransformer {
-	private static final float MIN_SCALE = 0.75f;
 
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public void transformPage(View view, float position) {
-		int pageWidth = view.getWidth();
-
-		if (position < -1) { // [-Infinity,-1)
-			// This page is way off-screen to the left.
-			view.setAlpha(0);
-
-		} else if (position <= 0) { // [-1,0]
-			// Use the default slide transition when moving to the left page
-			view.setAlpha(1);
-			view.setTranslationX(0);
-			view.setScaleX(1);
-			view.setScaleY(1);
-
-		} else if (position <= 1) { // (0,1]
-			// Fade the page out.
-			view.setAlpha(1 - position);
-
-			// Counteract the default slide transition
-			view.setTranslationX(pageWidth * -position);
-
-			// Scale the page down (between MIN_SCALE and 1)
-			float scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position));
-			view.setScaleX(scaleFactor);
-			view.setScaleY(scaleFactor);
-
-		} else { // (1,+Infinity]
-			// This page is way off-screen to the right.
-			view.setAlpha(0);
-		}
-	}
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
+	@Override
+    public void transformPage(View page, float position) {
+        float percentage = 1 - Math.abs(position);
+        page.setCameraDistance(12000);
+        setVisibility(page, position);
+        setTranslation(page);
+        setSize(page, position, percentage);
+        setRotation(page, position, percentage);
+    }
+ 
+    private void setVisibility(View page, float position) {
+        if (position < 0.5 && position > -0.5) {
+            page.setVisibility(View.VISIBLE);
+        } else {
+            page.setVisibility(View.INVISIBLE);
+        }
+    }
+ 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setTranslation(View page) {
+        ViewPager viewPager = (ViewPager) page.getParent();
+        int scroll = viewPager.getScrollX() - page.getLeft();
+        page.setTranslationX(scroll);
+    }
+ 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setSize(View page, float position, float percentage) {
+        page.setScaleX((position != 0 && position != 1) ? percentage : 1);
+        page.setScaleY((position != 0 && position != 1) ? percentage : 1);
+    }
+ 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setRotation(View page, float position, float percentage) {
+        if (position > 0) {
+            page.setRotationY(-180 * (percentage + 1));
+        } else {
+            page.setRotationY(180 * (percentage + 1));
+        }
+    }
 }
