277ee7d3728ef8ccccd5cd085ed0c848e71565ec
cesine <cesine@yahoo.com>
2014-09-12 12:01:01 -0400

    adding sample fragmentactivity
    from http://developer.android.com/training/implementing-navigation/lateral.html

--- sample/src/com/example/briangriffey/notebook/MainActivity.java
+++ sample/src/com/example/briangriffey/notebook/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.briangriffey.notebook;
 
+import com.briangriffey.notebook.PageTurnPagerAdapter;
+import com.briangriffey.notebook.PageTurnViewPager;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
+	// When requested, this adapter returns a DemoObjectFragment,
+	// representing an object in the collection.
+	PageTurnPagerAdapter mPagerAdapter;
+	PageTurnViewPager mViewPager;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		// ViewPager and its adapters use support library
+		// fragments, so use getSupportFragmentManager.
+		mPagerAdapter = new PageTurnPagerAdapter(getSupportFragmentManager());
+		mViewPager = (PageTurnViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mPagerAdapter);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
 }
